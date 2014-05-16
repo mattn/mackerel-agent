48bde5067b518634145a892dcd866b427d773065
mattn <mattn.jp@gmail.com>
2014-05-16 16:48:34 +0900

    Separate OS specific part for block device info

--- spec/block_device.go
+++ spec/block_device.go
@@ -1,11 +1,6 @@
 package spec
 
 import (
-	"io/ioutil"
-	"os"
-	"path"
-	"strings"
-
 	"github.com/mackerelio/mackerel-agent/logging"
 )
 
@@ -17,46 +12,3 @@ func (g *BlockDeviceGenerator) Key() string {
 }
 
 var blockDeviceLogger = logging.GetLogger("spec.block_device")
-
-func (g *BlockDeviceGenerator) Generate() (interface{}, error) {
-	fileInfos, err := ioutil.ReadDir("/sys/block")
-	if err != nil {
-		blockDeviceLogger.Errorf("Failed (skip this spec): %s", err)
-		return nil, err
-	}
-
-	results := make(map[string]map[string]interface{})
-
-	for _, fileInfo := range fileInfos {
-		deviceName := fileInfo.Name()
-		result := map[string]interface{}{}
-
-		for _, key := range []string{"size", "removable"} {
-			filename := path.Join("/sys/block", deviceName, key)
-			if _, err := os.Stat(filename); err == nil {
-				bytes, err := ioutil.ReadFile(filename)
-				if err != nil {
-					blockDeviceLogger.Errorf("Failed (skip this spec): %s", err)
-					return nil, err
-				}
-				result[key] = strings.TrimSpace(string(bytes))
-			}
-		}
-
-		for _, key := range []string{"model", "rev", "state", "timeout", "vendor"} {
-			filename := path.Join("/sys/block", deviceName, "device", key)
-			if _, err := os.Stat(filename); err == nil {
-				bytes, err := ioutil.ReadFile(filename)
-				if err != nil {
-					blockDeviceLogger.Errorf("Failed (skip this spec): %s", err)
-					return nil, err
-				}
-				result[key] = strings.TrimSpace(string(bytes))
-			}
-		}
-
-		results[fileInfo.Name()] = result
-	}
-
-	return results, nil
-}
