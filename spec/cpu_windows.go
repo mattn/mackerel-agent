13edfe9d0becf91293195184000de11de3785fea
mattn <mattn.jp@gmail.com>
2014-05-16 16:06:40 +0900

    Implement windows cpu info

--- spec/cpu_windows.go
+++ spec/cpu_windows.go
@@ -1,56 +1,125 @@
 package spec
 
 import (
-	"bufio"
-	"os"
-	"regexp"
+	"errors"
+	"fmt"
+	"syscall"
+	"unsafe"
 )
 
+const (
+	ERROR_SUCCESS      = 0
+	DRIVE_FIXED        = 3
+	HKEY_LOCAL_MACHINE = 0x80000002
+	RRF_RT_REG_SZ      = 0x00000002
+	RRF_RT_REG_DWORD   = 0x00000010
+	PDH_FMT_DOUBLE     = 0x00000200
+	PDH_INVALID_DATA   = 0xc0000bc6
+)
+
+var (
+	modkernel32 = syscall.NewLazyDLL("kernel32.dll")
+	modadvapi32 = syscall.NewLazyDLL("advapi32.dll")
+
+	procRegGetValue   = modadvapi32.NewProc("RegGetValueW")
+	procGetSystemInfo = modkernel32.NewProc("GetSystemInfo")
+)
+
+type SYSTEM_INFO struct {
+	ProcessorArchitecture     uint16
+	PageSize                  uint32
+	MinimumApplicationAddress *byte
+	MaximumApplicationAddress *byte
+	ActiveProcessorMask       *byte
+	NumberOfProcessors        uint32
+	ProcessorType             uint32
+	AllocationGranularity     uint32
+	ProcessorLevel            uint16
+	ProcessorRevision         uint16
+}
+
+func regGetString(hKey uint32, subKey string, value string) (string, error) {
+	var bufLen uint32
+	procRegGetValue.Call(
+		uintptr(hKey),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(subKey))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(value))),
+		uintptr(RRF_RT_REG_SZ),
+		0,
+		0,
+		uintptr(unsafe.Pointer(&bufLen)))
+	if bufLen == 0 {
+		return "", errors.New("Can't get size of registry value")
+	}
+
+	buf := make([]uint16, bufLen)
+	ret, _, err := procRegGetValue.Call(
+		uintptr(hKey),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(subKey))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(value))),
+		uintptr(RRF_RT_REG_SZ),
+		0,
+		uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(unsafe.Pointer(&bufLen)))
+	if ret != ERROR_SUCCESS {
+		return "", err
+	}
+
+	return syscall.UTF16ToString(buf), nil
+}
+
+func regGetInt(hKey uint32, subKey string, value string) (uint32, error) {
+	var num, numlen uint32
+	numlen = 4
+	ret, _, err := procRegGetValue.Call(
+		uintptr(hKey),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(subKey))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(value))),
+		uintptr(RRF_RT_REG_DWORD),
+		0,
+		uintptr(unsafe.Pointer(&num)),
+		uintptr(unsafe.Pointer(&numlen)))
+	if ret != ERROR_SUCCESS {
+		return 0, err
+	}
+
+	return num, nil
+}
+
 func (g *CPUGenerator) Generate() (interface{}, error) {
-	file, err := os.Open("/proc/cpuinfo")
-	if err != nil {
-		cpuLogger.Errorf("Failed (skip this spec): %s", err)
-		return nil, err
-	}
-
-	scanner := bufio.NewScanner(file)
-
 	results := make([]map[string]interface{}, 0)
-	var cur map[string]interface{}
-	for scanner.Scan() {
-		line := scanner.Text()
 
-		if matches := regexp.MustCompile(`^processor\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur = make(map[string]interface{})
-			results = append(results, cur)
-		} else if matches := regexp.MustCompile(`^vendor_id\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["vendor_id"] = matches[1]
-		} else if matches := regexp.MustCompile(`^cpu family\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["family"] = matches[1]
-		} else if matches := regexp.MustCompile(`^model\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["model"] = matches[1]
-		} else if matches := regexp.MustCompile(`^stepping\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["stepping"] = matches[1]
-		} else if matches := regexp.MustCompile(`^physical id\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["physical_id"] = matches[1]
-		} else if matches := regexp.MustCompile(`^core id\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["core_id"] = matches[1]
-		} else if matches := regexp.MustCompile(`^cpu cores\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["cores"] = matches[1]
-		} else if matches := regexp.MustCompile(`^model name\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["model_name"] = matches[1]
-		} else if matches := regexp.MustCompile(`^cpu MHz\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["mhz"] = matches[1]
-		} else if matches := regexp.MustCompile(`^cache size\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["cache_size"] = matches[1]
-		} else if matches := regexp.MustCompile(`^flags\s+:\s+(.*)$`).FindStringSubmatch(line); matches != nil {
-			cur["flags"] = regexp.MustCompile(` `).Split(matches[1], -1)
+	var systemInfo SYSTEM_INFO
+	procGetSystemInfo.Call(uintptr(unsafe.Pointer(&systemInfo)))
+
+	for i := uint32(0); i < systemInfo.NumberOfProcessors; i++ {
+		processorName, err := regGetString(
+			HKEY_LOCAL_MACHINE,
+			fmt.Sprintf(`HARDWARE\DESCRIPTION\System\CentralProcessor\%d`, i),
+			`ProcessorNameString`)
+		if err != nil {
+			return nil, err
 		}
+		processorMHz, err := regGetInt(
+			HKEY_LOCAL_MACHINE,
+			fmt.Sprintf(`HARDWARE\DESCRIPTION\System\CentralProcessor\%d`, i),
+			`~MHz`)
+		if err != nil {
+			return nil, err
+		}
+		vendorIdentifier, err := regGetString(
+			HKEY_LOCAL_MACHINE,
+			fmt.Sprintf(`HARDWARE\DESCRIPTION\System\CentralProcessor\%d`, i),
+			`VendorIdentifier`)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, map[string]interface{}{
+			"model_name": processorName,
+			"mhz":        processorMHz,
+			"model":      systemInfo.ProcessorArchitecture,
+			"vendor_id":  vendorIdentifier,
+		})
 	}
-	if err := scanner.Err(); err != nil {
-		cpuLogger.Errorf("Failed (skip this spec): %s", err)
-		return nil, err
-	}
-
 	return results, nil
 }
